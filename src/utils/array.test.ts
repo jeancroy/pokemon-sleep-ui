17d64940f45cb98257aeea97cce4a384bba81a65
RaenonX <raenonx0710@gmail.com>
2024-01-10 16:53:30 -0600

    Added `toAccumulated()`

--- src/utils/array.test.ts
+++ src/utils/array.test.ts
@@ -1,6 +1,6 @@
 import {describe, expect, it} from '@jest/globals';
 
-import {generateSegments} from '@/utils/array';
+import {generateSegments, toAccumulated} from '@/utils/array';
 
 
 describe('Array / Generate Segments', () => {
@@ -28,5 +28,25 @@ describe('Array / Generate Segments', () => {
     const result = [...generateSegments(2, [])];
 
     expect(result[0]).toStrictEqual([]);
+  });
+});
+
+describe('Array / To Accumulated', () => {
+  it('is correct', () => {
+    const result = [...toAccumulated([1, 2, 3, 4])];
+
+    expect(result[0]).toEqual(1);
+    expect(result[1]).toEqual(3);
+    expect(result[2]).toEqual(6);
+    expect(result[3]).toEqual(10);
+  });
+
+  it('is correct when it contains 0', () => {
+    const result = [...toAccumulated([1, 0, 0, 0])];
+
+    expect(result[0]).toEqual(1);
+    expect(result[1]).toEqual(1);
+    expect(result[2]).toEqual(1);
+    expect(result[3]).toEqual(1);
   });
 });
--- src/utils/array.ts
+++ src/utils/array.ts
@@ -2,6 +2,14 @@ export const toUnique = <T>(arr: T[]): T[] => Array.from(new Set(arr));
 
 export const toSum = (arr: number[]): number => arr.reduce((prev, curr) => prev + curr, 0);
 
+export function* toAccumulated(arr: number[]): Generator<number> {
+  let sum = 0;
+  for (const element of arr) {
+    sum += element;
+    yield sum;
+  }
+}
+
 export function* generateSegments<T>(size: number, arr: T[]): Generator<T[]> {
   for (let idx = 0; idx <= arr.length; idx++) {
     yield arr.slice(idx, idx + size);
