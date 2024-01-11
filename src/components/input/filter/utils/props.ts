2beb19c4e551af8a3563c0acd49ea8adb177b207
RaenonX <raenonx0710@gmail.com>
2024-01-11 05:15:18 -0600

    Added `isAllowed()` for `*selectOnClick` utils

--- src/components/input/filter/utils/props.ts
+++ src/components/input/filter/utils/props.ts
@@ -1,13 +1,13 @@
 import {FilterInputOnClickProps} from '@/components/input/filter/common/type';
-import {
-  FilterInclusionMap,
-  FilterWithInclusionMap,
-  FilterWithUpdaterProps,
-} from '@/components/input/filter/type';
+import {FilterInclusionMap, FilterWithInclusionMap, FilterWithUpdaterProps} from '@/components/input/filter/type';
 import {Indexable, KeysOfType} from '@/utils/type';
 
 
-export type GetSingleSelectOnClickPropsOpts<TFilter, TData> = FilterWithUpdaterProps<TFilter> & {
+type GetSelectOnClickCommonProps<TFilter> = FilterWithUpdaterProps<TFilter> & {
+  isAllowed?: () => boolean,
+};
+
+export type GetSingleSelectOnClickPropsOpts<TFilter, TData> = GetSelectOnClickCommonProps<TFilter> & {
   filterKey: KeysOfType<TFilter, TData | null>,
   allowNull?: boolean,
 };
@@ -15,20 +15,27 @@ export type GetSingleSelectOnClickPropsOpts<TFilter, TData> = FilterWithUpdaterP
 export const getSingleSelectOnClickProps = <TFilter, TData, TId>({
   filter,
   setFilter,
+  isAllowed,
   filterKey,
   allowNull = true,
 }: GetSingleSelectOnClickPropsOpts<TFilter, TData>): FilterInputOnClickProps<TId> => ({
   isActive: (id) => filter[filterKey] === id,
-  onClick: (id) => setFilter((original) => ({
-    ...original,
-    [filterKey]: original[filterKey] === id ? (allowNull ? null : id) : id,
-  } satisfies TFilter)),
+  onClick: (id) => {
+    if (isAllowed && !isAllowed()) {
+      return;
+    }
+
+    setFilter((original) => ({
+      ...original,
+      [filterKey]: original[filterKey] === id ? (allowNull ? null : id) : id,
+    } satisfies TFilter));
+  },
 });
 
 export type GetMultiSelectOnClickPropsOpts<
   TFilter extends FilterWithInclusionMap<TId>,
   TId extends Indexable
-> = FilterWithUpdaterProps<TFilter> & {
+> = GetSelectOnClickCommonProps<TFilter> & {
   filterKey: KeysOfType<TFilter, FilterInclusionMap<TId>>,
 };
 
@@ -38,16 +45,23 @@ export const getMultiSelectOnClickProps = <
 >({
   filter,
   setFilter,
+  isAllowed,
   filterKey,
 }: GetMultiSelectOnClickPropsOpts<TFilter, TId>): FilterInputOnClickProps<TId> => {
   return {
     isActive: (id) => !!filter[filterKey][id],
-    onClick: (id) => setFilter((original) => ({
-      ...original,
-      [filterKey]: {
-        ...original[filterKey],
-        [id]: !original[filterKey][id],
-      },
-    } satisfies TFilter)),
+    onClick: (id) => {
+      if (isAllowed && !isAllowed()) {
+        return;
+      }
+
+      setFilter((original) => ({
+        ...original,
+        [filterKey]: {
+          ...original[filterKey],
+          [id]: !original[filterKey][id],
+        },
+      } satisfies TFilter));
+    },
   };
 };
