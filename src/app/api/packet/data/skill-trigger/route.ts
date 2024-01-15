0e480075aeecd742882fd996babf2a4d2c33e17a
RaenonX <raenonx0710@gmail.com>
2024-01-14 19:04:17 -0600

    Fixed packet skill trigger rate data endpoint not returning data #687

--- src/app/api/packet/data/skill-trigger/route.ts
+++ src/app/api/packet/data/skill-trigger/route.ts
@@ -1,4 +1,7 @@
 import {handlePacketSkillTriggerRateDataRequest} from '@/handler/packet/data/skillTrigger';
+import {RouteDynamic} from '@/types/next/route';
 
+
+export const dynamic: RouteDynamic = 'force-dynamic';
 
 export const GET = handlePacketSkillTriggerRateDataRequest;
--- src/types/next/route.ts
+++ src/types/next/route.ts
@@ -1,3 +1,2 @@
 // https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config
-
 export type RouteDynamic = 'auto' | 'force-dynamic' | 'error' | 'force-static';
