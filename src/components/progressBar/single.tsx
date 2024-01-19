4cc5708020e5ef0abdcb58c40497c0780c4979dc
RaenonX <raenonx0710@gmail.com>
2024-01-19 10:01:18 -0600

    Updated `<ProgressBarSingle/>` to accept `classBar`

--- src/components/progressBar/single.tsx
+++ src/components/progressBar/single.tsx
@@ -5,18 +5,20 @@ import {clsx} from 'clsx';
 
 type Props = {
   percent: number,
-  className?: string,
+  classBar?: string,
   classBarHeight?: `h-${number}`,
   classRounding?: `rounded-${string}`,
   classRoundingOfBar?: `rounded-${string}`,
+  className?: string,
 };
 
 export const ProgressBarSingle = ({
   percent,
-  className,
+  classBar = 'bg-slate-500',
   classBarHeight = 'h-2.5',
   classRounding = 'rounded-full',
   classRoundingOfBar = 'rounded-full',
+  className,
 }: Props) => {
   return (
     <div className={clsx(
@@ -26,7 +28,8 @@ export const ProgressBarSingle = ({
       className,
     )}>
       <div style={{width: `${percent}%`}} className={clsx(
-        'transform-smooth bg-slate-500',
+        'transform-smooth',
+        classBar,
         classBarHeight,
         classRoundingOfBar,
       )}/>
