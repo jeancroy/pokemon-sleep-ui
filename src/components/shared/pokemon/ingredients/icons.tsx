baf33af5b975d2ed47e3c8699b803652b5b9748c
RaenonX <raenonx0710@gmail.com>
2024-01-19 03:53:49 -0600

    Updated `<PokemonIngredientIcons/>` to allow more customized styling

--- src/components/shared/pokemon/ingredients/icons.tsx
+++ src/components/shared/pokemon/ingredients/icons.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import {clsx} from 'clsx';
 
 import {Flex} from '@/components/layout/flex/common';
+import {FlexDirection} from '@/components/layout/flex/type';
 import {VerticalSplitter} from '@/components/shared/common/splitter';
 import {PokemonIngredientIcon} from '@/components/shared/pokemon/ingredients/icon';
 import {IngredientIconCommonProps} from '@/components/shared/pokemon/ingredients/type';
@@ -13,18 +14,27 @@ import {formatNumber} from '@/utils/number/format';
 
 type Props = IngredientIconCommonProps & {
   ingredients: IngredientProduction[][],
+  direction?: FlexDirection,
   numberFormat?: NumberFormat,
   className?: string,
+  classNameItem?: string,
 };
 
-export const PokemonIngredientIcons = ({ingredients, numberFormat = 'int', className, ...props}: Props) => {
+export const PokemonIngredientIcons = ({
+  ingredients,
+  direction = 'row',
+  numberFormat = 'int',
+  className,
+  classNameItem,
+  ...props
+}: Props) => {
   return (
-    <Flex direction="row" noFullWidth className={clsx(className ?? 'gap-0.5')}>
+    <Flex direction={direction} noFullWidth className={clsx(className ?? 'gap-0.5')}>
       {ingredients.map((data, idx) => (
         <React.Fragment key={data.map(({id, qty}) => `${id}x${qty}`).join('-')}>
           {idx !== 0 && <VerticalSplitter/>}
           {data.map(({id, qty}) => (
-            <Flex key={`${id}x${qty}`} direction="row" center noFullWidth className="gap-1">
+            <Flex key={`${id}x${qty}`} direction="row" center noFullWidth className={clsx(classNameItem ?? 'gap-1')}>
               <PokemonIngredientIcon id={id} {...props}/>
               <div>{formatNumber({format: numberFormat, num: qty})}</div>
             </Flex>
