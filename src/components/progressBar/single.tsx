f3562bf6337cb8fc1310d3068212f69a0840049a
RaenonX <raenonx0710@gmail.com>
2024-01-19 04:44:02 -0600

    Updated `<ProgressBarSingle/>` to accept `classRounding*`

--- src/components/progressBar/single.tsx
+++ src/components/progressBar/single.tsx
@@ -7,19 +7,29 @@ type Props = {
   percent: number,
   className?: string,
   classBarHeight?: `h-${number}`,
+  classRounding?: `rounded-${string}`,
+  classRoundingOfBar?: `rounded-${string}`,
 };
 
-export const ProgressBarSingle = ({percent, className, classBarHeight = 'h-2.5'}: Props) => {
+export const ProgressBarSingle = ({
+  percent,
+  className,
+  classBarHeight = 'h-2.5',
+  classRounding = 'rounded-full',
+  classRoundingOfBar = 'rounded-full',
+}: Props) => {
   return (
     <div className={clsx(
-      'transform-smooth w-full rounded-full bg-gray-400/50 dark:bg-gray-700/50',
+      'transform-smooth w-full bg-gray-400/50 dark:bg-gray-700/50',
       classBarHeight,
+      classRounding,
       className,
     )}>
-      <div
-        className={clsx('transform-smooth rounded-full bg-slate-500', classBarHeight)}
-        style={{width: `${percent}%`}}
-      />
+      <div style={{width: `${percent}%`}} className={clsx(
+        'transform-smooth bg-slate-500',
+        classBarHeight,
+        classRoundingOfBar,
+      )}/>
     </div>
   );
 };
