d311823832933aac3ba2eca6663d57d3a735b6bc
RaenonX <raenonx0710@gmail.com>
2024-01-25 17:46:25 -0600

    Added precise TTFP calculation impl #712

--- src/utils/game/producing/inventory/fullPackTime.ts
+++ src/utils/game/producing/inventory/fullPackTime.ts
@@ -1,14 +1,37 @@
 import {durationOfDay} from '@/const/common';
+import {EfficiencyInterval} from '@/types/game/stamina/efficiency';
 
 
 type GetSecondsToFullPackInSleepOpts = {
+  staminaIntervals: EfficiencyInterval[] | null,
+  // This daily count should NOT include efficiency multiplier
   dailyCount: number,
   carryLimit: number,
 };
 
 export const getSecondsToFullPackInSleep = ({
+  staminaIntervals,
   dailyCount,
   carryLimit,
-}: GetSecondsToFullPackInSleepOpts): number => {
-  return carryLimit / dailyCount * durationOfDay;
+}: GetSecondsToFullPackInSleepOpts): number | null => {
+  if (!staminaIntervals) {
+    return null;
+  }
+
+  let secsPassed = 0;
+  let spaceLeft = carryLimit;
+
+  for (const {efficiency, duration} of staminaIntervals) {
+    const maxQuantityInInterval = dailyCount * (duration / durationOfDay) * efficiency;
+
+    if (spaceLeft - maxQuantityInInterval <= 0) {
+      secsPassed += spaceLeft / maxQuantityInInterval * duration;
+      return secsPassed;
+    }
+
+    secsPassed += duration;
+    spaceLeft -= maxQuantityInInterval;
+  }
+
+  return null;
 };
