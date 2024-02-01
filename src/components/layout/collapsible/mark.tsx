118971679150e3c06f553ed8856b6a91f4afd596
RaenonX <raenonx0710@gmail.com>
2024-02-01 15:32:59 -0600

    Fixed incorrect color changing on hover for `<CollapsibleMark/>`

--- src/components/layout/collapsible/mark.tsx
+++ src/components/layout/collapsible/mark.tsx
@@ -13,7 +13,8 @@ export const CollapsibleMark = ({show}: Props) => {
   return (
     <div className={clsx(
       'transform-smooth group absolute bottom-1 right-1 size-3.5',
-      'stroke-black stroke-1 group-hover:stroke-white dark:stroke-white dark:group-hover:stroke-black',
+      'stroke-black stroke-1 enabled:group-hover:stroke-white',
+      'dark:stroke-white enabled:dark:group-hover:stroke-black',
     )}>
       {show ? <ChevronUpIcon/> : <ChevronDownIcon/>}
     </div>
