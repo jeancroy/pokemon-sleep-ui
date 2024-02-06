5ab61e48135e8ee93fd8c11425cb6cd4efd43a20
RaenonX <raenonx0710@gmail.com>
2024-02-06 06:46:35 -0600

    Updated team analysis to allow editing name #772
    closes #772

--- src/components/shared/team/member/popup/config.tsx
+++ src/components/shared/team/member/popup/config.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import {useTranslations} from 'next-intl';
 
+import {InputBox} from '@/components/input/box';
 import {Flex} from '@/components/layout/flex/common';
 import {PokemonIntentionalFullPackInput} from '@/components/shared/pokemon/inventory/fullPack/input';
 import {PokemonConfig} from '@/components/shared/pokemon/predefined/config/main';
@@ -13,13 +14,22 @@ export const TeamMemberConfig = (props: TeamMemberProps) => {
     member,
     setMember,
   } = props;
+  const {pokemonId, name} = member;
 
   const t = useTranslations('UI.UserSettings');
+  const t2 = useTranslations('Game');
 
   const alwaysFullPack = member.alwaysFullPack ?? null;
 
   return (
     <Flex noFullWidth className="gap-1.5 sm:w-[60vw]">
+      <InputBox
+        value={name ?? ''}
+        type="text"
+        placeholder={t2(`PokemonName.${pokemonId}`)}
+        className="w-full"
+        onChange={({target}) => setMember({name: target.value || null})}
+      />
       <PokemonConfig
         {...props}
         data={member}
