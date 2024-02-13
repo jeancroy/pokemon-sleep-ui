295b1fe0ec433e0edeaa85facbe93e823144ab00
John Mitchell-Grant <jgrant_dev@icloud.com>
2024-02-13 17:51:02 -0500

    Optimized Lint Step
    * Updated package.json for peer dependencies
    * Added concurrently to execute `next lint` based on extension
    * Updated lint-staged not to use `next lint` for js files outside of `src` dir

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,17 +1,43 @@
 const path = require('path');
 
+const micormatch = require('micromatch');
+
 
 const currentWorkingDir = process.cwd();
-const getFileList = (filenames) => filenames
-  .map((f) => path.relative(currentWorkingDir, f))
-  .join(' --file ');
 
-const nextJsLintCommand = (filenames) => `next lint --fix --file ${getFileList(filenames)}`;
-const eslintCommand = 'eslint --config ./.eslintrc.yml --ignore-path ./.eslintignore --fix';
+const nextJsLintArgs = '--fix --file';
+const getFileList = (filenames, isNextLint) => filenames
+  .map((f) => path.relative(currentWorkingDir, f))
+  .join(isNextLint ? ` ${nextJsLintArgs} ` : ' ');
+
+const nextJsLintCommand = (filenames) => (
+  `next lint . ${nextJsLintArgs} ${getFileList(filenames, true)}`
+);
+
+const eslintCommand = (filenames) => (
+  `eslint --cache --fix ${getFileList(filenames)}`
+);
+const tsJsLintCommand = (filenames) => {
+  const dotnetProjects = {
+    'src': micormatch(filenames, ['src/**']),
+    '*': micormatch(filenames, ['!src/**']),
+  };
+
+  return Object.entries(dotnetProjects).reduce((acc, [projectName, fileMatches]) => {
+    const eslintCmd = 'src' === projectName ? nextJsLintCommand : eslintCommand;
+
+    if (fileMatches.length) {
+      acc.push(`${eslintCmd(fileMatches)}`);
+    }
+
+    return acc;
+  }, []);
+};
 const stylelintCommand = 'stylelint --allow-empty-input --fix';
 
 module.exports = {
-  '*.{js,jsx,ts,tsx}': [nextJsLintCommand],
+  '*.{js,jsx}': [tsJsLintCommand],
+  '*.{ts,tsx}': [tsJsLintCommand],
   '*.{css,scss}': [stylelintCommand],
   '*.{json,yml}': [eslintCommand],
 };
