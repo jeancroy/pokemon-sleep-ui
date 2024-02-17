00ebe824c2ed3e0a73bfa6b18fb3f8117f091232
John Mitchell-Grant <jgrant_dev@icloud.com>
2024-02-17 10:50:33 -0500

    Resolved Peer Review Suggestions

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,26 +1,63 @@
 const path = require('path');
 
-const micormatch = require('micromatch');
+const micromatch = require('micromatch');
 
 
+/**
+ * @type {string} The stylelint command.
+ */
 const currentWorkingDir = process.cwd();
 
+/**
+ * @type {string} The `next lint` common args.
+ */
 const nextJsLintArgs = '--fix --file';
+
+/**
+ * Helper function to concatenate the files needed for commit linting.
+ *
+ * @param {string[]} filenames The file path names.
+ * @param {boolean} isNextLint Whether or not this is for NextJs Lint.
+ *
+ * @return {string} The command partial for files to lint.
+ */
 const getFileList = (filenames, isNextLint) => filenames
   .map((f) => path.relative(currentWorkingDir, f))
   .join(isNextLint ? ` ${nextJsLintArgs} ` : ' ');
 
+/**
+ * Helper function to generate the `next lint` command.
+ *
+ * @param {string[]} filenames The file path names.
+ *
+ * @return {string} The NextJs lint command.
+ */
 const nextJsLintCommand = (filenames) => (
   `next lint . ${nextJsLintArgs} ${getFileList(filenames, true)}`
 );
 
+/**
+ * Helper function to generate the `eslint` command for non-next linting.
+ *
+ * @param {string[]} filenames The file path names.
+ *
+ * @return {string} The eslint lint command.
+ */
 const eslintCommand = (filenames) => (
   `eslint --cache --fix ${getFileList(filenames)}`
 );
+
+/**
+ * Helper function to generate the lint commands for JS, TS, JSON and YML files.
+ *
+ * @param {string[]} filenames The file path names.
+ *
+ * @return {string[]} The lint commands for the nextjs and non-nextjs files.
+ */
 const tsJsLintCommand = (filenames) => {
   const dotnetProjects = {
-    'src': micormatch(filenames, ['src/**']),
-    '*': micormatch(filenames, ['!src/**']),
+    'src': micromatch(filenames, ['src/**']),
+    '*': micromatch(filenames, ['!src/**']),
   };
 
   return Object.entries(dotnetProjects).reduce((acc, [projectName, fileMatches]) => {
@@ -33,8 +70,16 @@ const tsJsLintCommand = (filenames) => {
     return acc;
   }, []);
 };
+
+/**
+ * @type {string} The stylelint command.
+ */
 const stylelintCommand = 'stylelint --allow-empty-input --fix';
 
+/**
+ * Color mixer.
+ * @module lint-staged/config
+ */
 module.exports = {
   '*.{js,jsx}': [tsJsLintCommand],
   '*.{ts,tsx}': [tsJsLintCommand],
