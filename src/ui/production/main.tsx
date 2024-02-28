7498f48c372ed93c2cd6ae848c39eb255ee6f39c
RaenonX <raenonx0710@gmail.com>
2024-02-28 02:07:08 -0600

    Fixed production comparison not loading saved data

--- src/ui/production/main.tsx
+++ src/ui/production/main.tsx
@@ -24,7 +24,7 @@ export const ProductionComparison = async ({params}: DefaultPageProps) => {
     teamMemberViewRequiredData,
   ] = await Promise.all([
     getFieldMetaMap(),
-    getUserProductionComparisonContent(),
+    getUserProductionComparisonContent(session?.user.id),
     getTeamMemberViewRequiredData(),
   ]);
 
