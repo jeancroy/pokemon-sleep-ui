df1d35aba98dec123fa2b76c6f5523ac1f7f4008
RaenonX <raenonx0710@gmail.com>
2024-03-02 00:27:23 -0600

    Updated max height of `<PopupBody/>`

--- src/components/popup/shared/body.tsx
+++ src/components/popup/shared/body.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 export const PopupBody = ({children}: React.PropsWithChildren<{}>) => {
   return (
-    <div className="max-h-[70vh] w-full overflow-y-auto p-2">
+    <div className="max-h-[85vh] w-full overflow-y-auto p-2">
       {children}
     </div>
   );
