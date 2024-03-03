389c80af4f1c7f5d99e6e1dcc9d14e1f6b91a10f
RaenonX <raenonx0710@gmail.com>
2024-03-02 22:57:53 -0600

    Refactored ingredient multiplier section of detailed production

---
+++ src/components/shared/pokemon/production/stats/tabs/content/cooking/ingredientMultiplier.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import {useTranslations} from 'next-intl';
+
+import {Flex} from '@/components/layout/flex/common';
+import {IngredientIcons} from '@/components/shared/ingredient/icons/main';
+import {PokemonDetailedProductionProps} from '@/components/shared/pokemon/production/stats/type';
+import {useCommonServerData} from '@/contexts/data/common/hook';
+import {formatFloat3} from '@/utils/number/format/regular';
+
+
+export const PokemonDetailedProductionCookingIngredientMultiplier = ({metadata}: PokemonDetailedProductionProps) => {
+  const {override, defaultValue} = metadata.ingredientMultiplier;
+
+  const {ingredientIds} = useCommonServerData();
+  const t = useTranslations('UI.Component.PokemonDetailedProduction.Cooking');
+
+  return (
+    <Flex className="info-section">
+      <div className="text-left text-lg">{t('IngredientMultiplier')}</div>
+      <IngredientIcons
+        ingredients={[ingredientIds.map((id) => ({id, qty: override[id] ?? defaultValue}))]}
+        dimension="size-6"
+        formatQty={(qty) => `${formatFloat3(qty)}x`}
+        classOfText="text-base"
+        classOfGap="gap-2"
+        getMark={({qty}) => qty > 1 ? 'green' : 'gray'}
+        wrapAll
+        wrapItem={false}
+      />
+    </Flex>
+  );
+};
--- src/components/shared/pokemon/production/stats/tabs/content/cooking/main.tsx
+++ src/components/shared/pokemon/production/stats/tabs/content/cooking/main.tsx
@@ -1,38 +1,18 @@
 import React from 'react';
 
-import {useTranslations} from 'next-intl';
-
-import {Flex} from '@/components/layout/flex/common';
-import {IngredientIcons} from '@/components/shared/ingredient/icons/main';
 import {
   PokemonDetailedProductionTabLayout,
 } from '@/components/shared/pokemon/production/stats/tabs/content/common/layout';
+import {
+  PokemonDetailedProductionCookingIngredientMultiplier,
+} from '@/components/shared/pokemon/production/stats/tabs/content/cooking/ingredientMultiplier';
 import {PokemonDetailedProductionProps} from '@/components/shared/pokemon/production/stats/type';
-import {useCommonServerData} from '@/contexts/data/common/hook';
-import {formatFloat3} from '@/utils/number/format/regular';
 
 
-export const PokemonDetailedProductionCooking = ({metadata}: PokemonDetailedProductionProps) => {
-  const {override, defaultValue} = metadata.ingredientMultiplier;
-
-  const {ingredientIds} = useCommonServerData();
-  const t = useTranslations('UI.Component.PokemonDetailedProduction.Cooking');
-
+export const PokemonDetailedProductionCooking = (props: PokemonDetailedProductionProps) => {
   return (
     <PokemonDetailedProductionTabLayout>
-      <Flex className="info-section">
-        <div className="text-left text-lg">{t('IngredientMultiplier')}</div>
-        <IngredientIcons
-          ingredients={[ingredientIds.map((id) => ({id, qty: override[id] ?? defaultValue}))]}
-          dimension="size-6"
-          formatQty={(qty) => `${formatFloat3(qty)}x`}
-          classOfText="text-base"
-          classOfGap="gap-2"
-          getMark={({qty}) => qty > 1 ? 'green' : 'gray'}
-          wrapAll
-          wrapItem={false}
-        />
-      </Flex>
+      <PokemonDetailedProductionCookingIngredientMultiplier {...props}/>
     </PokemonDetailedProductionTabLayout>
   );
 };
